b347c443ead06f15cdb80def09e33c651a195776
Thiago Goulart <thiago_gsg@hotmail.com>
2019-07-26 15:52:04 -0400

    Ajustes em NotificaÃ§Ãµes PWA. Atualizando lib firebase

--- js/solvesNotifications.js
+++ js/solvesNotifications.js
@@ -4,8 +4,9 @@
 **/
 function SolvesNotifications() {
   this.solvesPluginName = 'SolvesNotifications';
-  this.versionId = 1;
-  this.version = '1.0';
+  this.versionId = 2;
+  this.version = '1.1';
+  this.debug = false;
   this.icon = null;
   this.permission = null;
   this.actions = [];
@@ -16,9 +17,16 @@ function SolvesNotifications() {
   this.fireBasePermissionDivId = null;
   this.fireBaseMessagesDivId = null;
 
+  this.serverUrlNotifications =null; 
+  this.applicationServerKey = null;
+
   this.init = function(){
     $.Solves.addSolvesPlugin(this.solvesPluginName, $.SolvesNotifications);
   };
+  this.afterSolvesInit = function(){
+    this.debug = $.Solves.debug;
+    this.push_subscribe();
+  }
   this.destroy = function(){
     this.clearData();
   };
@@ -38,6 +46,12 @@ function SolvesNotifications() {
       console.log(this.solvesPluginName+' não encontrou firebaseConfig válida.');
     }
   }
+  this.getServerUrlNotifications = function(){
+    return this.serverUrlNotifications;
+  };
+  this.getApplicationServerKey = function(){
+    return this.applicationServerKey;
+  };
   this.setPublicVapidKey = function(key){
     this.fireBasePublicVapidKey = key;
   }
@@ -78,11 +92,12 @@ function SolvesNotifications() {
           this.mostraPwaNotification(title, json);
       }else if (Notification.permission !== "denied") {
           console.log("Notification permission IS NOT denied");
+          var _self = this;
           Notification.requestPermission().then(function (permission) {
             $.SolvesNotifications.permission = permission;
           console.log("Notification permission request: "+permission);
             if (permission === "granted") {
-              this.mostraPwaNotification(title, json);
+              _self.mostraPwaNotification(title, json);
             }
           });
       }
@@ -313,7 +328,144 @@ function SolvesNotifications() {
     }else{
       console.log(this.solvesPluginName+' não encontrou SolvesUi');
     }    
-  }
+  };
+  this.push_sendSubscriptionToServer = function(subscription, p_method) {
+    const key = subscription.getKey('p256dh');
+    const token = subscription.getKey('auth');
+    const contentEncoding = (PushManager.supportedContentEncodings || ['aesgcm'])[0];
+
+    return fetch(this.getServerUrlNotifications(), {
+      method: p_method,
+      body: JSON.stringify({
+        endpoint: subscription.endpoint,
+        publicKey: key ? btoa(String.fromCharCode.apply(null, new Uint8Array(key))) : null,
+        authToken: token ? btoa(String.fromCharCode.apply(null, new Uint8Array(token))) : null,
+        contentEncoding: contentEncoding
+      })
+    }).then(() => subscription);
+  };
+  this.push_subscribe = function() {
+    if(!$.Solves.isNotEmpty(this.getApplicationServerKey())){
+      return;
+    }
+    //changePushButtonState('computing');
+    var _self = this;
+    return this.checkNotificationPermission()
+      .then(() => navigator.serviceWorker.ready)
+      .then(serviceWorkerRegistration =>
+        serviceWorkerRegistration.pushManager.subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: _self.urlBase64ToUint8Array(this.getApplicationServerKey()),
+        })
+      )
+      .then(subscription => {
+        // Subscription was successful
+        // create subscription on your server
+        return _self.push_sendSubscriptionToServer(subscription, 'POST');
+      })
+      .catch(e => {
+        if (Notification.permission === 'denied') {
+          // The user denied the notification permission which
+          // means we failed to subscribe and the user will need
+          // to manually change the notification permission to
+          // subscribe to push messages
+          console.warn('Notifications are denied by the user.');
+          //changePushButtonState('incompatible');
+        } else {
+          // A problem occurred with the subscription; common reasons
+          // include network errors or the user skipped the permission
+          console.error('Impossible to subscribe to push notifications', e);
+         // changePushButtonState('disabled');
+        }
+      });
+  };
+  this.push_updateSubscription = function() {
+    var _self = this;
+    navigator.serviceWorker.ready
+      .then(serviceWorkerRegistration => serviceWorkerRegistration.pushManager.getSubscription())
+      .then(subscription => {
+     //  changePushButtonState('disabled');
+
+        if (!subscription) {
+          // We aren't subscribed to push, so set UI to allow the user to enable push
+          return;
+        }
+
+        // Keep your server in sync with the latest endpoint
+        return _self.push_sendSubscriptionToServer(subscription, 'PUT');
+      })
+      .then(subscription => subscription) // Set your UI to show they have subscribed for push messages
+      .catch(e => {
+        console.error('Error when updating the subscription', e);
+      });
+  };
+  this.checkNotificationPermission = function() {
+    return new Promise((resolve, reject) => {
+      if (Notification.permission === 'denied') {
+        return reject(new Error('Push messages are blocked.'));
+      }
+
+      if (Notification.permission === 'granted') {
+        return resolve();
+      }
+
+      if (Notification.permission === 'default') {
+        return Notification.requestPermission().then(result => {
+          if (result !== 'granted') {
+            reject(new Error('Bad permission result'));
+          }
+
+          resolve();
+        });
+      }
+    });
+  };
+  this.push_unsubscribe = function() {
+    var _self = this;
+    //changePushButtonState('computing');
+
+    // To unsubscribe from push messaging, you need to get the subscription object
+    navigator.serviceWorker.ready
+      .then(serviceWorkerRegistration => serviceWorkerRegistration.pushManager.getSubscription())
+      .then(subscription => {
+        // Check that we have a subscription to unsubscribe
+        if (!subscription) {
+          // No subscription object, so set the state
+          // to allow the user to subscribe to push
+          //changePushButtonState('disabled');
+          return;
+        }
+
+        // We have a subscription, unsubscribe
+        // Remove push subscription from server
+        return _self.push_sendSubscriptionToServer(subscription, 'DELETE');
+      })
+      .then(subscription => subscription.unsubscribe())
+      .then(() => changePushButtonState('disabled'))
+      .catch(e => {
+        // We failed to unsubscribe, this can lead to
+        // an unusual state, so  it may be best to remove
+        // the users data from your data store and
+        // inform the user that you have done so
+        console.error('Error when unsubscribing the user', e);
+        //changePushButtonState('disabled');
+      });
+  };
+  this.urlBase64ToUint8Array = function(base64String) {
+    if(!$.Solves.isNotEmpty(base64String)){
+      return null;
+    }
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding).replace(/\-/g, '+').replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  };
 }
 $.SolvesNotifications = new SolvesNotifications();
 $.SolvesNotifications.init();
